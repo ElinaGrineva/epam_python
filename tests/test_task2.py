d7c79522bb391f77f2566ce8953061d99d8de079
Elina Grineva <elina.grineva@yandex.ru>
2021-10-16 16:41:41 +0300

    added a 11hw

---
+++ tests/test_task2.py
@@ -0,0 +1,9 @@
+from homework11.task2 import ElderDiscount, MorningDiscount, Order
+
+
+def test_order():
+    order_1 = Order(100, MorningDiscount)
+    assert order_1.final_price() == 75
+
+    order_2 = Order(100, ElderDiscount)
+    assert order_2.final_price() == 85
